ba91c993fa1afe673471ed5807c139e078dd9359
Simon Sprünker <simon.spruenker@holisticon.de>
2022-02-03 14:32:03 +0100

    fix: comment

--- game-server/src/main/kotlin/com/example/demo/entity/GameEntity.kt
+++ game-server/src/main/kotlin/com/example/demo/entity/GameEntity.kt
@@ -3,7 +3,7 @@ package com.example.demo.entity
 import java.util.*
 import javax.persistence.Id
 
-// FIXME Add the correct annotations to this class and make
+// FIXME Add the correct annotations to this class and complete the constructor.
 data class GameEntity(
         @Id val id: String = Random().nextLong().toString()
 )
